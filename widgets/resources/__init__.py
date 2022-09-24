ff585365cb44b287581a2bb704d1073adeb976a4
mbiselx <michael.biselx@epfl.ch>
2022-09-24 18:52:47 +0200

    added resource module

---
+++ widgets/resources/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+The resource module for the project
+
+Author  :   Michael Biselx
+Date    :   09.2022
+Project :   PyQtTest
+'''
+
+# rename the generic file handlers
+from .images import get_path_to_file as get_path_to_img
