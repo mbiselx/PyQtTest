7bb2be506a8c74263619caeaa56bba3843e5e736
mbiselx <michael.biselx@epfl.ch>
2022-10-09 16:15:01 +0200

    added stylesheet resource

--- src/PyQtTest/resources/__init__.py
+++ src/PyQtTest/resources/__init__.py
@@ -10,7 +10,9 @@ import os
 
 # rename the generic file handlers
 from .images import get_path_to_file as get_path_to_img
+from .stylesheets import get_path_to_file as get_path_to_stylesheet
 
 resource_folder = os.sep.join(__file__.replace('/', os.sep).split(os.sep)[:-1])
 image_folder = os.sep.join([resource_folder, 'images'])
 image_label_folder = os.sep.join([resource_folder, 'image_labels'])
+stylesheet_folder = os.sep.join([resource_folder, 'stylesheets'])
