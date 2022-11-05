dbe7b214c1c301ca09351b4ea385eb1f380c0d3f
mbiselx <michael.biselx@epfl.ch>
2022-11-05 15:48:05 +0100

    the test file

---
+++ tests/test_formgeneration.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+'''
+A file used for testing
+
+Author  :   Michael Biselx
+Date    :   11.2022
+Project :   PyQtTest
+'''
+
+import sys
+import logging
+
+from PyQt5 import QtCore, QtWidgets
+
+from PyQtTest.widgets.utils.reloadable_widget import ReloadableWidget
+from PyQtTest.widgets.form_generation.form_generation import FormDisplay
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    print(f"running '{__file__.split('/')[-1]}' as Qt App")
+    app = QtWidgets.QApplication(sys.argv)
+
+    mw = ReloadableWidget(
+        flags=QtCore.Qt.WindowType.WindowStaysOnTopHint | QtCore.Qt.WindowType.WindowCloseButtonHint,
+        widget=FormDisplay,
+        form=r"C:\Users\Michael Biselx\Desktop\PyQtTest\src\PyQtTest\widgets\form_generation\form.json")
+    mw.show()
+    print("widget has been shown")
+
+    sys.exit(app.exec_())
